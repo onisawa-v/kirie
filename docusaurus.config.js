6fd85fa3fc397341db1125f155440ccb8265f6a5
onisawa <onisawa.v@gmail.com>
2023-02-10 01:57:32 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://onisawa-v.github.io/kirie',
+  url: 'https://onisawa-v.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/kirie/',
